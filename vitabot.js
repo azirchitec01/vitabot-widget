f05ef2eb4304f61a9b0ea068506944f4a5873c5b
azirchitec01 <azizecobatiment@gmail.com>
2025-07-26 10:27:59 +0100

    Create vitabot.js

---
+++ vitabot.js
@@ -0,0 +1,20 @@
+window.ChatWidgetConfig = {
+  webhook: {
+    url: 'https://flow.vitacucina.ma/webhook/8045fdf9-9516-4502-81d1-b1b26ebcdfaf/chat',
+    route: 'general'
+  },
+  branding: {
+    logo: 'https://vitacucina.ma/wp-content/uploads/elementor/thumbs/born-in-morocco-with-italien-excellence-bold-logo-r6gmnbmfhsz6hwv93tfbxez0ctypd0hrnd3jec8z18.png',
+    name: 'VitaBot',
+    welcomeText: 'Bonjour 👋 Je suis votre assistant intelligent',
+    responseTimeText: 'Réponse immédiate 24h/24',
+    floatingButtonText: 'Démarrer'
+  },
+  style: {
+    primaryColor: '#10b981',
+    secondaryColor: '#059669',
+    position: 'right',
+    backgroundColor: '#ffffff',
+    fontColor: '#1f2937'
+  }
+};
